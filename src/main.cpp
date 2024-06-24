f96095a2227f032461d8c774e995cd9c9e107aee
Moritz Werner <moritz.werner02@gmx.de>
2024-06-24 23:32:22 +0200

    versuch pumpe über pico zum laufen zu bringen

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-
 #include <Arduino.h>
+
+#define PUMP_PIN 18
+
 int moist;
+
+void pumpOn();
+void pumpOff();
+
 void setup()
 {
-  // initialize LED digital pin as an output.
-
+  pinMode(PUMP_PIN, OUTPUT);
+  digitalWrite(PUMP_PIN, LOW); // Initialize the pump as off
   Serial.begin(9600);
 }
 
@@ -20,7 +21,21 @@ void loop()
   Serial.print(">Moisture:");
   Serial.println(moist);
 
+  if (moist > 650){
+    pumpOn();
+    delay(5000);
+    pumpOff();
+    }
+   
 
 
   delay(100);
+}
+
+void pumpOn() {
+    digitalWrite(PUMP_PIN, HIGH);
+}
+
+void pumpOff() {
+    digitalWrite(PUMP_PIN, LOW);
 }
