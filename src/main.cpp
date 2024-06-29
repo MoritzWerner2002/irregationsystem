a09582945d6cc97d9f63efc803df75611191b414
Moritz Werner <moritz.werner02@gmx.de>
2024-06-29 15:00:08 +0200

    RTC ausprobiert

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
-
+#include <Wire.h>
+#include "RTClib.h"
+#include <SPI.h>
 #define PUMP_PIN 18
+
+RTC_DS3231 rtc;
 
 int moist;
 
@@ -11,6 +15,14 @@ void setup()
 {
   pinMode(PUMP_PIN, OUTPUT);
   digitalWrite(PUMP_PIN, LOW); // Initialize the pump as off
+  Wire.begin();
+  rtc.begin();
+
+  // Prüfen, ob die RTC läuft
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
   Serial.begin(9600);
 }
 
@@ -21,13 +33,27 @@ void loop()
   Serial.print(">Moisture:");
   Serial.println(moist);
 
-  if (moist > 650){
+  /*if (moist > 650){
     pumpOn();
     delay(5000);
     pumpOff();
-    }
+    }*/
    
+  DateTime now = rtc.now();
 
+  Serial.print("Datum: ");
+  Serial.print(now.day());
+  Serial.print('/');
+  Serial.print(now.month());
+  Serial.print('/');
+  Serial.print(now.year());
+  Serial.print(" - Zeit: ");
+  Serial.print(now.hour());
+  Serial.print(':');
+  Serial.print(now.minute());
+  Serial.print(':');
+  Serial.print(now.second());
+  Serial.println();
 
   delay(100);
 }
